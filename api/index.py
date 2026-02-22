1c5058d80f3e02ebcb437e9c83500979b27d18c7
luckline <50654848+luckline@users.noreply.github.com>
2026-02-22 14:51:29 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,9 @@
-from flask import Flask
+from http.server import BaseHTTPRequestHandler
 
-# 变量名必须叫 app，这是 Vercel 的默认寻找对象
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Laitest Tech Python API is Live!"
-
-@app.route('/api/test')
-def test():
-    return {"status": "success", "message": "Backend connected"}
-
-# 关键修复：显式导出，防止 Vercel 运行时识别失败
-# 在文件最底部添加这一行
-handler = app
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Laitest Tech Backend is working!'.encode('utf-8'))
+        return
